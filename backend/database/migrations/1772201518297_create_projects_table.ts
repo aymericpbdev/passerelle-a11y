06bdc031b21aeb268740befa7cbf2434ca4fe5c0
aymericpbdev <pain_blavec@hotmail.fr>
2026-02-27 15:20:16 +0100

    Migration create_projects_table

---
+++ backend/database/migrations/1772201518297_create_projects_table.ts
@@ -0,0 +1,30 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'projects'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table
+        .integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE')
+
+      table.string('name').notNullable()
+      table.string('url, 2048').notNullable()
+      table.boolean('is_public').defaultTo(false).notNullable()
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
